c799dd1c531c80887d183c19a87d64cf9e9d3685
mbrhdrg01 <96500032+mbrhdrg01@users.noreply.github.com>
2023-12-30 16:45:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ else:
     LOG_CHANNEL = int(LOG_CHANNEL)
 
 # support group
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', 'MalluMoviesGroup01')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1001755407281')
 if len(SUPPORT_GROUP) == 0:
     print('Error - SUPPORT_GROUP is missing, exiting now')
     exit()
